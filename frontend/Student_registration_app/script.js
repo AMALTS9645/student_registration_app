6e570b8025f765d6a1914f73cc19382b981dd771
Amal Thekkel Sobhanakumari <Amal.ThekkelSobhanakumari@ust.com>
2023-10-17 02:24:42 +0530

    front end forms

---
+++ frontend/Student_registration_app/script.js
@@ -0,0 +1,85 @@
+const addBtn = document.querySelector("#add");
+const input = document.querySelector(".card");
+
+
+const registerButton = document.createElement("button");
+registerButton.type = "submit";
+registerButton.classList.add("btn", "btn-primary", "btn-block");
+registerButton.textContent = "Register";
+
+
+function addInput() {
+  const newForm = document.createElement("form");
+  const firstNameLabel = document.createElement("label");
+  const firstName = document.createElement("input");
+  const lastNameLabel = document.createElement("label");
+  const lastName = document.createElement("input");
+  const ageLabel = document.createElement("label");
+  const age = document.createElement("input");
+  const departmentNameLabel = document.createElement("label");
+  const departmentName = document.createElement("input");
+  const card = document.createElement("div");
+  const separator = document.createElement("hr");
+
+
+  const removeButton = document.createElement("button");
+  removeButton.type = "button";
+  removeButton.classList.add("btn", "btn-danger", "btn-block");
+  removeButton.textContent = "Remove";
+  removeButton.addEventListener("click", () => {
+    input.removeChild(card);
+  });
+
+  newForm.id = "registration-form";
+  newForm.classList.add("form-group");
+  firstNameLabel.for = "firstName";
+  firstNameLabel.textContent = "First Name:";
+  firstName.type = "text";
+  firstName.placeholder = "Enter first name";
+  firstName.id = "firstName";
+  firstName.classList.add("form-control", "my-3");
+
+  lastNameLabel.for = "lastName";
+  lastNameLabel.textContent = "Last Name:";
+  lastName.type = "text";
+  lastName.id = "lastName";
+  lastName.placeholder = "Enter last name";
+  lastName.classList.add("form-control", "my-3");
+
+  ageLabel.for = "age";
+  ageLabel.textContent = "Age:";
+  age.type = "number";
+  age.id = "age";
+  age.placeholder = "Enter Age";
+  age.classList.add("form-control", "my-3");
+
+  departmentNameLabel.for = "departmentName";
+  departmentNameLabel.textContent = "Department Name:";
+  departmentName.type = "text";
+  departmentName.id = "departmentName";
+  departmentName.placeholder = "Enter departmentName";
+  departmentName.classList.add("form-control", "my-3");
+
+  card.classList.add("card-body");
+
+
+  newForm.appendChild(firstNameLabel);
+  newForm.appendChild(firstName);
+  newForm.appendChild(lastNameLabel);
+  newForm.appendChild(lastName);
+  newForm.appendChild(ageLabel);
+  newForm.appendChild(age);
+  newForm.appendChild(departmentNameLabel);
+  newForm.appendChild(departmentName);
+  newForm.appendChild(removeButton);
+  newForm.appendChild(separator)
+
+
+  newForm.appendChild(registerButton);
+// ____________________________________________________
+  card.appendChild(newForm);
+
+  input.appendChild(card);
+}
+
+addBtn.addEventListener("click", addInput);
