b732c23386838e7ab08d0e6f1e6b5caf29622b43
Amal Thekkel Sobhanakumari <Amal.ThekkelSobhanakumari@ust.com>
2023-10-18 16:29:41 +0530

    api-gateway-server added

--- backend/api-gateway-server/src/main/java/com/apigateway/ApiGatewayServerApplication.java
+++ backend/api-gateway-server/src/main/java/com/apigateway/ApiGatewayServerApplication.java
@@ -1,17 +1,15 @@
-package com.studentregister;
+package com.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableDiscoveryClient
-public class StudentRegistrationServerApplication {
+public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentRegistrationServerApplication.class, args);
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
 	}
 
 }
